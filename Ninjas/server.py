c170b78e89c4bed56e8ad2db90dcd4a86354c867
Raymond Reid <rayreid76@gmail.com>
2018-05-08 09:20:41 -0700

    ninja finished

--- Ninjas/server.py
+++ Ninjas/server.py
@@ -9,4 +9,19 @@ def index():
 def tmnt():
     return render_template('ninja.html')
 
+@app.route('/result', methods=['POST'])
+def results():
+    color = request.form['color']
+    if color == 'blue':
+        print(color)
+        return render_template('result.html')
+    elif color == 'red':
+        return render_template('rap.html')
+    elif color == 'orange':
+        return render_template('mich.html')
+    elif color == 'purple':
+        return render_template('don.html')
+    else:
+        return render_template('no_ninja.html')
+
 app.run(debug=True)
