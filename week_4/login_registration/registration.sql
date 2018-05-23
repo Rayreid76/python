b1bccf896691e5f468bf6a04726ea130fe560a56
Raymond Reid <rayreid76@gmail.com>
2018-05-23 15:08:06 -0700

    finish login

--- week_4/login_registration/registration.sql
+++ week_4/login_registration/registration.sql
@@ -1,4 +1,3 @@
 SELECT * FROM login_registration1.users;
 
-alter table users
-add user_name varchar(255);
+INSERT INTO users (`first_name`, `last_name`, `password`, `created_at`, `updated_at`, `user_name`, `email`) VALUES ('ray', 'reid', 'hhshhh', now(), now(), 'todo', 'ray@town.com');
--- week_4/login_registration/server.py
+++ week_4/login_registration/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, render_template,flash, request, session
 from mysqlconnection import MySQLConnector
-import re
+import re, md5
+password = 'password'
+
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app = Flask(__name__)
 app.secret_key = 'askjdfhaisdgf'
@@ -9,6 +11,15 @@ mysql = MySQLConnector(app,'login_registration1')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['post'])
+def login():
+    password = md5.new(request.form['pword']).hexdigest()
+    uname = request.form['uname']
+    user_query = "SELECT * FROM users where users.user_name = :uname AND users.password = :pword"
+    query_data = { 'uname': uname, 'pword': password}
+    user = mysql.query_db(user_query, query_data)
+    return redirect('/user')
 
 @app.route('/reg')
 def regist():
@@ -19,7 +30,7 @@ def user():
     user = mysql.query_db("select * from users")
     return render_template('user.html', all_user=user)
 
-@app.route('/registration', methods=["post"])
+@app.route('/registration', methods=['post'])
 def create():
     if len(request.form['uname']) < 1:
         flash("User Name is blank!!!")
@@ -27,36 +38,33 @@ def create():
     elif not EMAIL_REGEX.match(request.form['email']):
         flash("Not valid email")
         return redirect('/reg')
-    elif len(request.form['pword']) < 1:
+    elif len(request.form['pword']) <= 8:
         flash("Password is blank")
         return redirect('/reg')
-    elif len(request.form['lname']) < 1:
+    elif len(request.form['lname']) <= 2:
         flash("Last name is blank")
         return redirect('/reg')
-    elif len(request.form['fname']) < 1:
+    elif len(request.form['fname']) <= 2:
         flash("First name is blank")
         return redirect('/reg')
     else:
-        flash("Success! You are registured")
-    print("start")    
+        flash("Success! You are registured")   
     uname = request.form["uname"]
     email = request.form["email"]
-    pword = request.form["pword"]
+    pword = md5.new(request.form["pword"]).hexdigest()
     fname = request.form["fname"]
     lname = request.form["lname"]
-    print("values")
     query = "INSERT INTO users (`first_name`, `last_name`, `password`, `created_at`, `updated_at`, `user_name`, `email`) VALUES (:fname, :lname, :pword, now(), now(), :uname, :email);"
-    print("insert")
     data = {
-             'user_name':uname,
+             'uname':uname,
              'email':email,
-             'password':pword,
-             'first_name':fname,
-             'last_name':lname
+             'pword':pword,
+             'fname':fname,
+             'lname':lname
             }
-    print("data")
     user = mysql.query_db(query, data)
-    print("mysql")
-    return redirect('/user')       
+    return redirect('/user')
+
+   
 
 app.run(debug=True)
