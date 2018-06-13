353d13a69ae67b9e49642614a244411838377739
Raymond Reid <rayreid76@gmail.com>
2018-06-13 08:21:07 -0700

    survey finished

--- week_6/survey_form/main/apps/survey/views.py
+++ week_6/survey_form/main/apps/survey/views.py
@@ -4,6 +4,10 @@ def index(request):
     return render(request,"survey/index.html")
 
 def results(request):
+    request.session["name"] = request.POST["name"]
+    request.session["location"] = request.POST["location"]
+    request.session["lang"] = request.POST["lang"]
+    request.session["comments"] = request.POST["comments"]
     return render(request, "survey/results.html")
     
 
