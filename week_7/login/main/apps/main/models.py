84193dd266b47a6b4b9dab1328be082f1d71cff9
Raymond Reid <rayreid76@gmail.com>
2018-07-13 13:00:08 -0700

    login 1

--- week_7/login/main/apps/main/models.py
+++ week_7/login/main/apps/main/models.py
@@ -1,5 +1,46 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+class userManage(models.Manager):
+    def userValidation(self, fname, lname, email, password, cpassword):
+        valid = True
+        errors = []
+        if len(fname) <= 3:
+            valid = False
+            errors.append("Error first name is to short")
+        elif fname.isalpha() == False:
+            valid = False
+            errors.append("Error only letters")
+        if len(lname) <= 3:
+            valid = False
+            errors.append("Error last name is to short")
+        elif lname.isalpha() == False:
+            valid = False
+            errors.append("Error only letters")
+        if email != "":
+            try:
+                validate_email(email)
+            except ValidationError as e:
+                valid = False
+                errors.append("Error not valid email")
+        if len(password) <= 8:
+            valid = False
+            errors.append("Error password needs to be 8 characters")
+        if cpassword != password:
+            valid = False
+            errors.append("Error passward does not match")
+        else:
+            valid = True
+
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    objects = userManage()
 
 # Create your models here.
--- week_7/login/main/apps/main/views.py
+++ week_7/login/main/apps/main/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render, HttpResponse, redirect
+from models import * 
+
+
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 # Create your views here.
 def index(request):
     return render(request,"main/index.html")
 
 def register(request):
+    fname = request.POST["first_name"]
+    lname = request.POST["last_name"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+    cpassword = request.POST["confirm_password"]
+    valid = True
+    if len(fname) <= 3:
+        valid = False
+        print ("Error first name is to short")
+    elif fname.isalpha() == False:
+        valid = False
+        print("Error only letters")
+    if len(lname) <= 3:
+        valid = False
+        print ("Error last name is to short")
+    elif lname.isalpha() == False:
+        valid = False
+        print("Error only letters")
+    if email != "":
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            valid = False
+            print "Error not valid email"
+        else:
+            print "hooray! email is valid"
+    if len(password) <= 8:
+        valid = False
+        print("Error password needs to be 8 characters")
+    if cpassword != password:
+        valid = False
+        print("Error passward does not match")
+    else:
+        Users.objects.create(first_name=fname, last_name=lname, email=email, password=password)
     return redirect("/")
 
 def login(request):
     return redirect("/")
+
+    #r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$'
